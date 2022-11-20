dbdb3d4d47901dcb013f16b12cc1ecba22ccea18
jrcomitre <gcomitrejr@gmail.com>
2022-11-20 20:23:10 -0300

    get and set

---
+++ Orientação a Objetos/heranca.js
@@ -0,0 +1,86 @@
+/* 
+funcionarios
+nome,
+sobrenome,
+email,
+cpf,
+função,
+registro
+
+clientes
+nome,
+sobrenoms,
+email,
+cpf,
+renda
+
+funções
+descrição,
+salário
+*/
+
+class Funcao{
+    constructor(descricao, salario){
+        this._descricao = descricao;
+        this._salario = salario;
+    }
+
+    get descricao(){
+        return this._descricao;
+    }
+
+    set descricao(descricao){
+        this._descricao = descricao;
+    }
+
+    get salario(){
+        return this._salario;
+    }
+
+    set salario(salario){
+        this._salario = salario;
+    }
+}
+
+class Pessoa{
+    constructor(nome, sobrenome, email, cpf){
+        this._nome = nome;
+        this._sobrenome = sobrenome;
+        this._email = email;
+        this._cpf = cpf;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    set nome(nome){
+        this._nome = nome;
+    }
+
+    get sobrenome(){
+        return this._sobrenome;
+    }
+
+    set sobrenome(sobrenome){
+        this._sobrenome = sobrenome;
+    }
+
+    get email(){
+        return this._email;
+    }
+
+    set email(email){
+        this._email = email;
+    }
+
+    get cpf(){
+        return this.cpf;
+    }
+
+    set cpf(cpf){
+        this._cpf = cpf;
+    }
+
+
+}
