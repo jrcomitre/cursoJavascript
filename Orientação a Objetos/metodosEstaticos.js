d96c3ac6086a193c2087897fcc42fab8ab8b777c
jrcomitre <gcomitrejr@gmail.com>
2022-11-23 19:08:01 -0300

    Metodos Estaticos Inicio

---
+++ Orientação a Objetos/metodosEstaticos.js
@@ -0,0 +1,18 @@
+class Carro{
+    constructor(modelo){
+        this._marca = 'Honda';
+        this._modelo = modelo;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    get modelo(){
+        return this._modelo;
+    }
+
+    set modelo(modelo){
+        this._modelo;
+    }
+}
