66de8d53e8e301fcdee751fb64c93d5438d36117
jrcomitre <gcomitrejr@gmail.com>
2023-01-01 11:45:09 -0300

    Dom

---
+++ DOM/html/js/script.js
@@ -0,0 +1,13 @@
+let a = document.querySelector('.link');
+
+// console.log(a.innerText);
+
+// console.log(a.parentNode); //body
+
+// console.log(a.parentNode.parentNode); //html
+
+// console.log(a.parentNode.parentNode.parentNode) //Document
+
+console.log(a.parentNode.parentNode.parentNode .parentNode) //null
+
+console.log(a.parentNode.parentNode.childNodes)
