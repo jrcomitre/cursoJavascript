3ba6fab87dd116daec907fba43da425200907324
jrcomitre <gcomitrejr@gmail.com>
2023-01-03 10:15:12 -0300

    Alterando estilos final

---
+++ DOM/Alterando estilos/scriptb.js
@@ -0,0 +1,8 @@
+let btn = document.querySelectorAll('button');
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].onclick = function(){
+        document.querySelector('body').style.backgroundColor = this.getAttribute('class').split('-')[1];
+        
+    };    
+};
