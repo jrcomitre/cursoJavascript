9c84fd29b64c5d990ba8733bbaba238ca737efae
jrcomitre <gcomitrejr@gmail.com>
2023-01-02 21:23:50 -0300

    Elementos da pagina exemplo 3

---
+++ DOM/Elementos da pagina/scriptb.js
@@ -0,0 +1,9 @@
+function removerBotao(btn){
+    // Recuperar o elemento div
+    // let divElement = document.querySelector('#app');
+
+    //Remove o filho selecionado da div
+    // divElement.removeChild(btn);
+
+    let divElement = document.querySelector('#app').removeChild(btn);
+}
