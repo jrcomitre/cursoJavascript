fd04ea089a13af8be688f8a68b83b9af4f8ec2e9
Gilberto Comitre Junior <gcomitrejr@gmail.com>
2024-02-25 18:42:27 -0300

    Promisses - Final

--- JavaScript Assincrono/Requisicao AJAX/promisses.js
+++ JavaScript Assincrono/Requisicao AJAX/promisses.js
@@ -5,7 +5,58 @@ let promisse = function(){
     return new Promise(function(resolve, reject){
         let github_user = document.querySelector('input[name=github_user]').value;
         let ajax = new XMLHttpRequest();
-        ajax.apen('GET', `https://api.github.com/users/${input.value}`);
+        ajax.open('GET', `https://api.github.com/users/${github_user}`);
         ajax.send(null);
+
+        ajax.onreadystatechange = function(){
+            if(ajax.readyState === 4){
+                if(ajax.status === 200){
+                    resolve(JSON.parse(ajax.responseText));
+                }else{
+                    reject('Não foi encontrado nenum usuário com este nome.')
+                }
+            }
+        }
     });
+}
+
+btn.onclick = function () {
+
+    // Limpar conteúdo da div
+    div.innerHTML = '';
+
+    // Criar o span
+    let spanNome = document.createElement('span');
+
+    // Criar a variável nome
+    let txtNome = '';
+
+    // Executando a promisse
+    promisse()
+        // Se o usuário tem nome
+        .then(function(response){
+            // Se o usuário tem nome
+            if(response['name'] !== null){
+                txtNome = document.createTextNode(response['name']);
+
+                let img = document.createElement('img');
+                img.setAttribute('src', response['avatar_url']);
+                img.setAttribute('alt', response['name']);
+                img.setAttribute('width', '45px');
+                img.setAttribute('height', '45px');
+                
+                div.appendChild(img);                
+            }else{
+                txtNome = document.createTextNode('O usuário encontrado não possui nome');
+            }
+            // Adiciona o texto ao span e a span a div
+            spanNome.appendChild(txtNome);
+            div.appendChild(spanNome);
+        })
+        .catch(function(error){
+            txtNome = document.createTextNode(error);
+            // Adiciona o texto ao span e o span a div
+            spanNome.appendChild(txtNome);
+            div.appendChild(spanNome);            
+        });
 }
